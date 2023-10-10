d055080cc2d962c0e1f1082a84d52d9d0c655fac
Muhammad Talha <talhashabbir9@gmail.com>
2023-10-11 04:45:31 +0500

    formatting

--- db/db_setup.py
+++ db/db_setup.py
@@ -4,17 +4,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@localhost/sample_ecommerce"  
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@localhost/sample_ecommerce"
 
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, future=True
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, future=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
 
 
 Base = declarative_base()
+
 
 # DB Utilities
 def get_db():
